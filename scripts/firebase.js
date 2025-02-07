bad5203f36d5a3ba61939355573da45da3946093
devhorn <mdevhorn@gmail.com>
2025-02-07 12:17:39 +0100

    added firebase functions

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -16,6 +16,23 @@ async function postData(path, data = {}) {
 }
 
 /**
+ * This function updates a specific object in firebase
+ *
+ * @param {string} path - This the path to the object in firebase for example (path = `users/${users[i].firebaseid}`),
+ * @param {string} data - this is the object for example data = { id: editedId, username: editedUsername }
+ */
+
+async function putData(path, data = {}) {
+  await fetch(URL + path + ".json", {
+    method: "PUT",
+    header: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+}
+
+/**
  * This function deletes a specific entrie in firebase
  *
  * @param {string} path - This the path to a specific object in firebase for example `/users/${users[i].firebaseid}`
