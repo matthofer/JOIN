779dd7cad9a5ee36aab25b248f55eb0735c9200b
AlxRpp <alexanderr26@outlook.de>
2025-02-09 15:21:42 +0100

    buttons done add AddTask

--- scripts/addTask.js
+++ scripts/addTask.js
@@ -3,7 +3,7 @@ let categorys = ["Technical Task", "User Story", "Customer Support", "Bug Fix"]
 async function initAddTask() {
     await loadContactsData();
     renderDropdownContacts();
-
+    changePrio('medium', 'mediumSVG')
 }
 
 function toggleDropdown(id) {
@@ -20,7 +20,7 @@ function toggleDropdown(id) {
 
 function renderDropdownContacts() {
     let html = document.getElementById('contacts');
-    contacts.sort((a,b) => a.name.localeCompare(b.name));
+    contacts.sort((a, b) => a.name.localeCompare(b.name));
     for (let contactIndex = 0; contactIndex < contacts.length; contactIndex++) {
         const singleContact = contacts[contactIndex];
         html.innerHTML += contactDropdownTemplate(singleContact, contactIndex)
@@ -52,7 +52,7 @@ function getSelectedContacts() {
     })
 }
 
-function filterContacts(){
+function filterContacts() {
     let valueInput = document.getElementById('contactsDropdown').value.toUpperCase();
     let filteredContacts = contacts.filter(contact => contact.name.toUpperCase().includes(valueInput))
     if (filteredContacts.length > 0) {
@@ -65,11 +65,31 @@ function filterContacts(){
     }
 }
 
-function renderFilteredContact(filteredContacts){
+function renderFilteredContact(filteredContacts) {
     let html = document.getElementById('contacts');
     html.innerHTML = "";
     for (let index = 0; index < filteredContacts.length; index++) {
         const element = filteredContacts[index];
         html.innerHTML += contactDropdownTemplate(element, index)
+    }
+}
+
+
+function changePrio(id, svg) {
+    let buttons = ["urgent", "medium", "low"];
+    let svgs = ["urgentSVG", "mediumSVG", "lowSVG"];
+    buttons.forEach(btn => document.getElementById(btn).classList.remove("urgent", "medium", "low"));
+    svgs.forEach(svgId => document.getElementById(svgId).classList.remove("urgentSVG", "mediumSVG", "lowSVG"))
+    let button = document.getElementById(id);
+    let img = document.getElementById(svg)
+    if (id == "urgent") {
+        button.classList.add('urgent')
+        img.classList.add('urgentSVG')
+    } else if (id == "medium") {
+        button.classList.add('medium')
+        img.classList.add('mediumSVG')
+    } else {
+        button.classList.add('low')
+        img.classList.add('lowSVG')
     }
 }
