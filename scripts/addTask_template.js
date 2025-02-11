c15591b322924033d87103cbb154e287f0f22558
AlxRpp <alexanderr26@outlook.de>
2025-02-11 22:07:51 +0100

    subtasks are Done add Task

--- scripts/addTask_template.js
+++ scripts/addTask_template.js
@@ -21,15 +21,21 @@ function categorysDropdownTemplate(categorys, categorysIndex) {
 
 function subtasksTemplate(subtask, subtasksIndex) {
     return `
-    <div ondblclick="editSubtask(${subtasksIndex})" onmouseover="showSubtaskMenu(${subtasksIndex})" onmouseleave="closeSubtaskMenu(${subtasksIndex})" class="singleSubtask" id="singleSubtask${subtasksIndex}">
+    <div ondblclick="editSubtask(${subtasksIndex})" onmouseover="showSubtaskMenu(${subtasksIndex})" 
+    onmouseleave="closeSubtaskMenu(${subtasksIndex})" class="singleSubtask hover" id="singleSubtask${subtasksIndex}">
         <div class="singleSubtaskLeft">
             <div class="dot"></div>
             <span id="editSpan${subtasksIndex}">${subtask}</span>
         </div>
         <div  class="singleSubtaskMenu d-none" id="singleSubtaskMenu${subtasksIndex}">
-            <img src="./assets/icons/edit_contact.svg">
+            <img onclick="editSubtask(${subtasksIndex})" src="./assets/icons/edit_contact.svg">
             <div class="subtaskMenuDivider"></div>
             <img onclick="deleteSubtask(${subtasksIndex})" src="./assets/icons/trash.svg">
+        </div>
+        <div class="editMenu d-none" id="editMenu${subtasksIndex}">
+            <img onclick="deleteSubtask(${subtasksIndex})" src="./assets/icons/trash.svg">
+            <div class="subtaskMenuDivider"></div>
+            <img onclick="saveEditedSubtask(${subtasksIndex})" src="./assets/icons/checkBlue.svg" alt="" srcset="">
         </div>
     </div>
     `;
