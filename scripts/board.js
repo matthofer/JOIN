4009cc5447ac45e03cf4ffe686f80eee58cb99d1
devhorn <mdevhorn@gmail.com>
2025-02-11 16:20:11 +0100

    create dummy data for task

---
+++ scripts/board.js
@@ -0,0 +1,32 @@
+let tasks = [
+  {
+    category: "user story",
+    title: "Kochwelt Page & Recipe Recommender",
+    dscr: "Build start page with recipe recommendation...",
+    subtasks: [
+      { id: 1, dscr: "build", done: true },
+      { id: 2, dscr: "test", done: false },
+      { id: 3, dscr: "deploy", done: false },
+    ],
+    contacts: [
+      { name: "Max Meyer", firebaseID: "12345" },
+      { name: "Tom Taler", firebaseID: "678910" },
+      { name: "Susi Seger", firebaseID: "101113" },
+    ],
+    prio: "medium",
+  },
+  {
+    category: "Technical Task",
+    title: "CSS Architecture Planning",
+    dscr: "Define CSS naming conventions and structure...",
+    subtasks: [
+      { id: 1, dscr: "build", done: true },
+      { id: 2, dscr: "test", done: false },
+    ],
+    contacts: [
+      { name: "Bernd Bürger", firebaseID: "12345" },
+      { name: "Nils Fischer", firebaseID: "678910" },
+    ],
+    prio: "low",
+  },
+];
