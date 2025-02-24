46c8762c123cede6fd3011d11ea7e8ac05ac0e08
devhorn <mdevhorn@gmail.com>
2025-02-24 21:43:49 +0100

    commenting board.js

--- scripts/board.js
+++ scripts/board.js
@@ -71,6 +71,13 @@ function fillTasksArray(taskResponseToJson, taskKey) {
   });
 }
 
+/**
+ * This function renders the tasks in the respective board column.
+ * To do this, it runs through the global array taskCategorys and filters the respective tasks by category from the global tasks.
+ *
+ * @param {array} taskArray - the global tasks array
+ *
+ */
 function renderTasks(taskArray) {
   for (let index = 0; index < taskCategorys.length; index++) {
     let category = taskArray.filter(
@@ -126,7 +133,7 @@ function renderContactIntials(task, i) {
 }
 
 /**
- * This function return the Intials to the contacts of the tasks
+ * This function return the Intials of the contact
  *
  * @param {string} contact - the name of the contact
  *
@@ -233,6 +240,12 @@ function updateStatusBar(amountDone, amountSubTasks, task, i) {
   }`;
 }
 
+/**
+ * This function open the add task overlay in the board and loads functionality of the add task page
+ *
+ * @param {string} type - the type in which column the task will be added e.g. todo
+ *
+ */
 async function openAddTaskOverlay(type) {
   document.getElementById("overlayAddTask").innerHTML = getAddTaskTemplate();
   let form = document.getElementById("addTaskForm");
@@ -242,10 +255,20 @@ async function openAddTaskOverlay(type) {
   await loadTasksData();
 }
 
+/**
+ * This function close the add task overlay
+ *
+ */
 function closeAddTaskOverlay() {
   document.getElementById("overlayAddTask").classList.add("overlayClosed");
 }
 
+/**
+ * This function open the task details and renders the corrosponding task informations
+ *
+ * @param {number} i - index of the global tasks array
+ *
+ */
 function openTaskDetails(i) {
   document.getElementById("taskEditContainer").innerHTML = "";
   document.getElementById("taskEditContainer").innerHTML =
@@ -255,6 +278,12 @@ function openTaskDetails(i) {
   document.getElementById("overlayEditTask").classList.remove("overlayClosed");
 }
 
+/**
+ * This function rendering all tasks details when the overlay is open
+ *
+ * @param {number} i - index of the global tasks array
+ *
+ */
 function renderTaskDetails(i) {
   document.getElementById("taskEditContainer").innerHTML = "";
   document.getElementById("taskEditContainer").innerHTML =
@@ -263,6 +292,11 @@ function renderTaskDetails(i) {
   renderSubTasksInDetail(i);
 }
 
+/**
+ * This function closes the edit task overlay. By closing the
+ * By closing, the tasks in the board are rendered and thereby updated
+ *
+ */
 async function closeEditTaskOverlay() {
   subtasks = [];
   statusSubtasks = [];
@@ -272,6 +306,12 @@ async function closeEditTaskOverlay() {
   document.getElementById("overlayEditTask").classList.add("overlayClosed");
 }
 
+/**
+ * This function renders the contacts in the task details
+ *
+ * @param {number} i - index of the global tasks array
+ *
+ */
 function renderContactsInTaskDetail(i) {
   document.getElementById("detailContactsList").innerHTML = "";
   if (tasks[i].contacts != undefined) {
@@ -288,6 +328,12 @@ function renderContactsInTaskDetail(i) {
   }
 }
 
+/**
+ * This function renders the subtasks in the task details
+ *
+ * @param {number} i - index of the global tasks array
+ *
+ */
 async function renderSubTasksInDetail(i) {
   await loadTasksData();
   document.getElementById("subtasksList").innerHTML = "";
@@ -305,6 +351,13 @@ async function renderSubTasksInDetail(i) {
   }
 }
 
+/**
+ * This function deletes the task in firebase.
+ * After that the tasks data will be loaded, the board renderd, the overlay closed and a message displayed
+ *
+ * @param {number} i - index of the global tasks array
+ *
+ */
 async function deleteTask(i) {
   await deleteData(`/tasks/${tasks[i].firebaseid}`);
   await loadTasksData();
@@ -313,6 +366,12 @@ async function deleteTask(i) {
   showMessage("Task successfully deleted");
 }
 
+/**
+ * This function search a specific task corrosponding to the user input
+ *
+ * @param {string} id - id of the search input html tag
+ *
+ */
 function searchTask(id) {
   clearColumnContent();
   let searchInputRef = document.getElementById(id);
@@ -328,6 +387,13 @@ function searchTask(id) {
   }
 }
 
+/**
+ * This function returns the search result.
+ * It searches for description and title. The two arrays with the search results are then concatenated and returned as one array
+ *
+ * @param {string} searchInput - value of the search input field
+ *
+ */
 function getSearchedTasks(searchInput) {
   let filteredTitleTasks = tasks.filter((task) =>
     task.title.toLowerCase().includes(searchInput.toLowerCase())
@@ -343,6 +409,11 @@ function getSearchedTasks(searchInput) {
   return filteredTasks;
 }
 
+/**
+ * This function clears the column content.
+ * Is needed before rendering the search result.
+ *
+ */
 function clearColumnContent() {
   document.getElementById("todo").innerHTML = "";
   document.getElementById("inProgress").innerHTML = "";
@@ -350,6 +421,11 @@ function clearColumnContent() {
   document.getElementById("done").innerHTML = "";
 }
 
+/**
+ * This function renders in the task detail dialog the edit template with all functionalities of add task
+ *
+ * @param {number} i - index of the global tasks array
+ */
 async function editTask(i) {
   document.getElementById("taskEditContainer").innerHTML = "";
   document.getElementById("taskEditContainer").innerHTML = editTaskTemplate(i);
@@ -364,6 +440,11 @@ async function editTask(i) {
   renderSubtasks();
 }
 
+/**
+ * This function pushes the contacts of the task in the global array selected contacts
+ *
+ * @param {number} i - index of the global tasks array
+ */
 function getSelectedContacts(i) {
   selectedContacts = [];
   if (tasks[i].contacts != undefined) {
@@ -382,6 +463,11 @@ function getSelectedContacts(i) {
   }
 }
 
+/**
+ * This function marks the selected Contacts in the drop down
+ *
+ * @param {array} selectedContacts - the global array selected contacts
+ */
 function markCheckBoxInEdit(selectedContacts) {
   for (let i = 0; i < selectedContacts.length; i++) {
     let contactIndex = contacts.findIndex(
@@ -395,6 +481,12 @@ function markCheckBoxInEdit(selectedContacts) {
   }
 }
 
+/**
+ * This function makes it possible to check the subtasks in task details and update it in firebase
+ *
+ * @param {number} i - index of the global tasks array
+ * @param {number} statusIndex - the index of the subtask in the task
+ */
 async function checkSubtask(i, statusIndex) {
   let checkbox = document.getElementById("subtask" + i + statusIndex);
   if (checkbox.checked) {
@@ -404,6 +496,12 @@ async function checkSubtask(i, statusIndex) {
   }
 }
 
+/**
+ * This function pushes the subtask in and his status in the global arrays subtasks and statusSubtasks
+ *
+ * @param {number} i - index of the global tasks array
+ *
+ */
 function getSubtasks(i) {
   subtasks = [];
   if (tasks[i].subtasks != undefined) {
@@ -417,6 +515,12 @@ function getSubtasks(i) {
   }
 }
 
+/**
+ * This function updates the task in firebase if validation is ok
+ *
+ * @param {number} i - index of the global tasks array
+ *
+ */
 async function updateTask(i) {
   let title = document.getElementById("title").value.length;
   let date = document.getElementById("date").value.length;
@@ -434,6 +538,12 @@ async function updateTask(i) {
   }
 }
 
+/**
+ * This function collects all values from the input fields and returns it in a object
+ *
+ * @param {number} i - index of the global tasks array
+ *
+ */
 function collectEditTaskData(i) {
   let title = document.getElementById("title").value;
   let description = document.getElementById("description").value;
@@ -452,6 +562,10 @@ function collectEditTaskData(i) {
   return data;
 }
 
+/**
+ * This function collects the subtask in in edit mode returns it in a object
+ *
+ */
 function getEditSubtasksForFB() {
   checkSubtaskStatusArray();
   let subtasksOBJ = {};
@@ -464,6 +578,11 @@ function getEditSubtasksForFB() {
   return subtasksOBJ;
 }
 
+/**
+ * This function checks if the subtasks and statusSubtasks array have the same length.
+ * If not the statusSubtasks array will be filled up with the false value.
+ *
+ */
 function checkSubtaskStatusArray() {
   if (subtasks.length != statusSubtasks.length) {
     let difference = subtasks.length - statusSubtasks.length;
@@ -475,11 +594,24 @@ function checkSubtaskStatusArray() {
   }
 }
 
+/**
+ * This function opens in mobile view the dialog to move the task to another category.
+ *
+ * @param {number} i - index of the global tasks array
+ *
+ */
 function openCloseTaskMoveDialogMobile(i) {
   let dialogRef = document.getElementById("moveDialog" + tasks[i].id);
   dialogRef.classList.toggle("dNone");
 }
 
+/**
+ * This function moves the task to another category.
+ *
+ * @param {number} taskId - id of the task
+ * @param {string} category - category where task should be moved to
+ *
+ */
 function moveToInMobile(taskId, category) {
   tasks[taskId].type = category;
   renderTasks(tasks);
